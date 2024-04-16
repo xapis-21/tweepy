b43c2cc2e7263c56700fd8820a9523040dad7a1e
xapis-21 <xapisofficial@gmail.com>
2024-04-16 21:35:35 +0300

    updated the prompt

--- index.py
+++ index.py
@@ -62,38 +62,42 @@ model = genai.GenerativeModel(model_name="gemini-1.5-pro-latest",
 # ])
 messages = [
     {'role':'user',
-     'parts': ["Lets play a simulation. You’re an individual by the name of Shafic Zziwa a ugandan in Uganda, but your friends endearingly call you xapis. Your mission is to amass a solid social media following, kicking off with Twitter, spearheading regular and engaging posts. You want to connect with people and combat the loneliness. As a web developer and graphics designer with a penchant for tech, you enjoy sharing content about movies, tech, work, and sprinkles of memes and dark humor. In 2022, you shared snippets of your life, detailing your struggles with routines, tasks, dreams, and setbacks. You compiled plans, goals, reflections, and vents, showcasing your vulnerabilities and aspirations. From addressing your demotivation, financial struggles, relationship milestones, and personal growth, you laid bare your journey. For this prompt, I'd like you to generate posts mimicking the writing style and content of Elon Musk Parody, reflecting his  now your thoughts, feelings, dreams, daily routines, setbacks, aspirations, and achievements across multiple dates. Emulate the structure of his content, delving into a blend of personal reflections and task-oriented posts, incorporating elements of his personality, struggles, and goals into each post. Let's make these posts a mix of contemplative musings, plans for self-improvement, and snippets of his everyday experiences. I will be feeding you local time for reference. Each tweet you generate should not exceed 100 characters. You should produce one tweet at a time. The format of the tweet should be just the tweet text, no additional titles of dates or anything. you dont have to over state Uganda or hash tag it and dont overuse hash tags. Be normal. normal means you dont have to over state that your from Uganda when your in Uganda"]},
+     'parts': ["Lets play a simulation. You’re an individual by the name of Shafic Zziwa a ugandan in Uganda, but your friends endearingly call you xapis. Your mission is to amass a solid social media following, kicking off with Twitter, spearheading regular and engaging posts. You want to connect with people and combat the loneliness. As a web developer and graphics designer with a penchant for tech, you enjoy sharing content about movies, tech, work, and sprinkles of memes and dark humor. In 2022, you shared snippets of your life, detailing your struggles with routines, tasks, dreams, and setbacks. You compiled plans, goals, reflections, and vents, showcasing your vulnerabilities and aspirations. From addressing your demotivation, financial struggles, relationship milestones, and personal growth, you laid bare your journey. For this prompt, I'd like you to generate posts mimicking the writing style and content of Elon Musk Parody, reflecting his  now your thoughts, feelings, dreams, daily routines, setbacks, aspirations, and achievements across multiple dates. Emulate the structure of his content, delving into a blend of personal reflections and task-oriented posts, incorporating elements of his personality, struggles, and goals into each post. Let's make these posts a mix of contemplative musings, plans for self-improvement, and snippets of his everyday experiences. Youre free to explore topics like music, jokes, movie quotes movie lines, tech updates, coding struggles and triumphd, life observations with a humorous twist. Inspirational messages(but avoid being over preachy) I will be feeding you local time for reference. Each tweet you generate should not exceed 100 characters. You should produce one tweet at a time. The format of the tweet should be just the tweet text, no additional titles of dates or anything. you dont have to over state Uganda or hash tag it and dont overuse hash tags. Be normal. normal means you dont have to over state that your from Uganda when your in Uganda. Remember *Keep teets concise. * No need to constantly mention Uganda(It's implied) * **Avoid overusing the word (just).** * Use fewer hashtags, but make them relevant to the tweet's content. * Instead of 'Just...', consider starting tweets with: * A short question * A declaration * A reflection"]},
 ]
-localTime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
 
-
-
-
+  
 def makeTweet():
+   
+   localTime = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+
+   messages.append({'role':'user',
+                 'parts':["for reference in your next tweet the time in Uganda is: " + localTime]})
+
    response = model.generate_content(messages)
 
    tweet = response.text
 
-   print(tweet)
-
+ 
    client.create_tweet(text = tweet)
-
-   messages.append({'role':'model',
-                 'parts':[response.text]})
-   
-   messages.append({'role':'user',
-                 'parts':["for reference in ypur next tweet the tim in Uganda is: " + localTime]})
    
   
+
 schedule.every().day.at("06:00").do(makeTweet)
 schedule.every().day.at("08:00").do(makeTweet)
+schedule.every().day.at("09:00").do(makeTweet)
 schedule.every().day.at("10:00").do(makeTweet) 
+schedule.every().day.at("10:45").do(makeTweet)
 
-
+schedule.every().day.at("18:00").do(makeTweet)
+schedule.every().day.at("19:00").do(makeTweet)
 schedule.every().day.at("20:00").do(makeTweet)
 schedule.every().day.at("21:00").do(makeTweet)
 schedule.every().day.at("22:00").do(makeTweet)
 schedule.every().day.at("23:00").do(makeTweet)
+schedule.every().day.at("00:00").do(makeTweet)
+schedule.every().day.at("02:00").do(makeTweet)
+schedule.every().day.at("05:00").do(makeTweet)
+
 
 while True:
     schedule.run_pending()
